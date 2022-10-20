9073e42a6592b6973e4973031a336f6db18fa2b9
NamDo8467 <huynhnamdo@gmail.com>
2022-10-20 14:11:13 -0400

    finish signup and login for user

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,17 +1,45 @@
 const express = require("express")
 const route = express.Router()
+const { User } = require("../models/User")
+route.post("/signup", async (req, res) => {
+	let { username, email, password } = req.body
+	const user = new User({ username, email, password })
 
-route.post("/signup", (req, res) => {
-	// let a= req.body.sdf
-	
-	
+	let response
+	try {
+		const result = await user.save()
+		response = result
+		res.status(201)
+	} catch (error) {
+		response = {
+			status: "false",
+			message: "username/email has already been taken"
+		}
+		res.status(400)
+	}
+	res.send(response)
+	// console.log(user)
 })
 
-
-route.post("/login", (req, res) => {
-	// let a= req.body.sdf
-	
-	
+route.post("/login", async (req, res) => {
+	let { username, password } = req.body
+	let response
+	try {
+		let user = await User.findOne({ username, password })
+		response = {
+			status: true,
+			username: user.username,
+			message: "User logged in successfully"
+		}
+		res.status(200)
+	} catch (error) {
+		response = {
+			status: false,
+			message: "Invalid Username and password"
+		}
+		res.status(400)
+	}
+	res.send(response)
 })
 
 module.exports.userRouter = route
