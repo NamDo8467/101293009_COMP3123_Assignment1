7d3efd3dd0f6affa3491697ed1f1a3e5b408474b
NamDo8467 <huynhnamdo@gmail.com>
2022-11-25 09:51:08 -0500

    change header

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 5000 || process.env.PORT
 const { userRouter } = require("./routes/userRoute")
 const { employeeRouter } = require("./routes/employeeRoute")
 
-const URL = "http://localhost:3000"  
+const URL = "http://localhost:3000"
 
 // app.use(cors({ origin: URL, optionsSuccessStatus: 200, credentials: true }))
 
@@ -13,7 +13,8 @@ app.set("trust proxy", 1)
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", URL)
 	res.setHeader("Access-Control-Allow-Credentials", true)
-	res.header(
+
+	res.setHeader(
 		"Access-Control-Allow-Headers",
 		"Origin,X-Requested-With,X-HTTP-Method-Override,,Content-Type,Accept,content-type,application/json"
 	)
@@ -25,7 +26,7 @@ app.use(express.json())
 
 mongoose.connect(process.env.DB_CONNECTION_LINK, { useNewUrlParser: true.valueOf(), useUnifiedTopology: true }, err => {
 	if (err) {
-		console.log(err);
+		console.log(err)
 	}
 })
 
