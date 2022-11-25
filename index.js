28bf52b0045c8555367f23a808ef49f799700c79
NamDo8467 <huynhnamdo@gmail.com>
2022-11-25 10:06:54 -0500

    allow headers authorization

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.set("trust proxy", 1)
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", URL)
 	res.setHeader("Access-Control-Allow-Credentials", true)
+	res.setHeader("Access-Control-Allow-Headers", "Authorization")
 	res.setHeader(
 		"Access-Control-Allow-Headers",
 		"Origin,X-Requested-With,X-HTTP-Method-Override,,Content-Type,Accept,content-type,application/json"
