d86486db7d8c34bdd6f423760712f1a95b5541f4
NamDo8467 <huynhnamdo@gmail.com>
2022-11-25 21:55:27 -0500

    change from delete to post

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -89,7 +89,7 @@ route
 		res.send(response)
 	})
 
-route.delete("/employees/delete", async (req, res) => {
+route.post("/employees/delete", async (req, res) => {
 	let id = req.body.id
 	let response = {}
 	try {
